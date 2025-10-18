3c5f94eef7f1b3fef0dba3e5a6ec78f44c9f47af
Pavisha <pavishapalanivel1004@gmail.com>
2025-10-18 22:08:45 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  this.reset();
+});
